e16a167705ac5cbe56c69097e38f70faeb9cc273
atsuki-kitahori <pcp.m.t15@gmail.com>
2024-09-28 14:26:24 +0900

    絞り込み機能の実装

--- src/public/mypage.php
+++ src/public/mypage.php
@@ -7,11 +7,14 @@ $pdo = new PDO(
     $dbPassword
 );
 
-$sql = 'SELECT * FROM pages';
-$statement = $pdo->prepare($sql);
-$statement->bindValue(':title', $title, PDO::PARAM_STR);
-$statement->bindValue(':content', $content, PDO::PARAM_STR);
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+$order = isset($_GET['order']) ? $_GET['order'] : 'desc';
 
+$sql =
+    'SELECT * FROM pages WHERE (name LIKE :search OR contents LIKE :search) ORDER BY created_at ' .
+    ($order === 'asc' ? 'ASC' : 'DESC');
+$statement = $pdo->prepare($sql);
+$statement->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
 $statement->execute();
 $pages = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -23,25 +26,37 @@ $pages = $statement->fetchAll(PDO::FETCH_ASSOC);
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>top画面</title>
+  <title>マイページ</title>
 </head>
 
 <body>
   <div>
-
     <div>
-      <form action="index.php" method="get">
+      <form action="mypage.php" method="get">
+        <div>
+          <input type="text" name="search" value="<?php echo htmlspecialchars(
+              $search,
+              ENT_QUOTES,
+              'UTF-8'
+          ); ?>" placeholder="検索ワード">
+        </div>
         <div>
           <label>
-            <input type="radio" name="order" value="desc" class="">
+            <input type="radio" name="order" value="desc" <?php echo $order ===
+            'desc'
+                ? 'checked'
+                : ''; ?>>
             <span>新着順</span>
           </label>
           <label>
-            <input type="radio" name="order" value="asc" class="">
+            <input type="radio" name="order" value="asc" <?php echo $order ===
+            'asc'
+                ? 'checked'
+                : ''; ?>>
             <span>古い順</span>
           </label>
         </div>
-        <button type="submit">送信</button>
+        <button type="submit">検索</button>
       </form>
     </div>
     
@@ -54,9 +69,21 @@ $pages = $statement->fetchAll(PDO::FETCH_ASSOC);
         </tr>
         <?php foreach ($pages as $page): ?>
           <tr>
-            <td><?php echo $page['name']; ?></td>
-            <td><?php echo $page['contents']; ?></td>
-            <td><?php echo $page['created_at']; ?></td>
+            <td><?php echo htmlspecialchars(
+                $page['name'],
+                ENT_QUOTES,
+                'UTF-8'
+            ); ?></td>
+            <td><?php echo htmlspecialchars(
+                $page['contents'],
+                ENT_QUOTES,
+                'UTF-8'
+            ); ?></td>
+            <td><?php echo htmlspecialchars(
+                $page['created_at'],
+                ENT_QUOTES,
+                'UTF-8'
+            ); ?></td>
           </tr>
         <?php endforeach; ?>
       </table>
